9dbeaadbb03f04871aaf2b6a83298916dc391827
aksupaul852131 <aksupaul.in@gmail.com>
2023-02-10 23:43:53 +0530

    a15

--- pages/tools/get/[id].js
+++ pages/tools/get/[id].js
@@ -17,25 +17,24 @@ export default function Tool() {
     const [tools, setTools] = useState();
     const [postType, setPostType] = useState(0);
 
+    const [dbKey, setDbKey] = useState('acGasRefilling');
+
     useEffect(() => {
         (() => getResponse())();
     });
 
     const getResponse = async () => {
         const urlSearchParams = new URLSearchParams(window.location.search)
-        const url = urlSearchParams.get('key');
+        setDbKey(urlSearchParams.get('key'));
 
         if(loading2) {
             const docRef = doc(db, "tools", 'LhzKS84prEU2TjwHsup1');
             const docSnap = await getDoc(docRef);
 
             if(docSnap.exists()) {
-                if(url == 'acInstall') {
-                    setTools(docSnap.data().acInstall);
-                } else if(url == 'acGasRefilling') {
-                    setTools(docSnap.data().acGasRefilling);
-                } else if(url == 'ideaIDU') {
-                    setTools(docSnap.data().ideaIDU);
+                setTools(docSnap.data()[dbKey]);
+
+                if(dbKey == 'iduInstallIdea' || dbKey == 'oduInstallIdea') {
                     setPostType(1);
                 }
                 setLoading2(false);
@@ -57,7 +56,7 @@ export default function Tool() {
 
     const onCheckHandle = () => {
 
-        if(tools.filter(i => i.pos == 'act').map((e) => (e.name)).length > 0) {
+        if(tools?.filter(i => i.pos == 'act').map((e) => (e.name)).length > 0) {
             setPopup(true);
         } else {
             toast.custom((t) => (
@@ -107,98 +106,98 @@ export default function Tool() {
     return (
         <>
             {loading ? <Loading />
-                :
-                <div className="pt-6 w-full font-[Urbanist] select-none">
-                    {
-                        postType == 1 && (
-                            <div className="relative mx-2 mb-4 ">
-                                <div className="absolute right-0 text-white bg-primary rounded-md flex items-center px-6 h-full cursor-pointer ">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="icon icon-tabler icon-tabler-mail"
-                                        width={16}
-                                        height={16}
-                                        viewBox="0 0 24 24"
-                                        strokeWidth="2"
-                                        stroke="currentColor"
-                                        fill="none"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
-                                    </svg>
+                : tools ?
+                    <div className="pt-6 w-full font-[Urbanist] select-none">
+                        {
+                            postType == 1 && (
+                                <div className="relative mx-2 mb-4 ">
+                                    <div className="absolute right-0 text-white bg-primary rounded-md flex items-center px-6 h-full cursor-pointer ">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            className="icon icon-tabler icon-tabler-mail"
+                                            width={16}
+                                            height={16}
+                                            viewBox="0 0 24 24"
+                                            strokeWidth="2"
+                                            stroke="currentColor"
+                                            fill="none"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        >
+                                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
+                                        </svg>
+                                    </div>
+                                    <input
+                                        onChange={(e) => onSearch(e.target.value)}
+                                        className="w-full bg-white dark:bg-dim-400 border-gray-200 dark:border-dim-400 text-gray-100 focus:bg-gray-100 dark:focus:bg-dim-900 focus:outline-none focus:border focus:border-blue-200 font-normal h-14 md:h-16 flex items-center pl-4 py-2 text-sm rounded-md border"
+                                        placeholder="Search Twitter"
+                                    />
                                 </div>
-                                <input
-                                    onChange={(e) => onSearch(e.target.value)}
-                                    className="w-full bg-white dark:bg-dim-400 border-gray-200 dark:border-dim-400 text-gray-100 focus:bg-gray-100 dark:focus:bg-dim-900 focus:outline-none focus:border focus:border-blue-200 font-normal h-14 md:h-16 flex items-center pl-4 py-2 text-sm rounded-md border"
-                                    placeholder="Search Twitter"
-                                />
-                            </div>
-                        )
-                    }
-                    {
-                        postType == 1 ?
-                            <ul className="grid grid-cols-2 px-3 gap-3">
-                                {tools.map((e, index) => (
-                                    <li className="relative" onClick={() => setPopup2(`${e.img}`)}>
-
-                                        <img src={e.img} className='rounded-lg h-32 w-full object-cover filter-none contrast-900' />
-                                        <h2 className="absolute bottom-2 left-2 text-sm">{e.name}</h2>
-
-                                    </li>
-                                ))}
-                            </ul>
-                            :
-                            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-3 md:px-32">
-                                {
-                                    tools.map((e, index) => (
-                                        <div
-                                            onClick={() => {
-                                                handlechange(index);
-                                            }}
-                                            key={index}
-                                            className={`${e.pos == 'act' && `border-primary border-2`} grid items-center justify-items-center border-2 p-4 md:p-6 rounded-lg`}>
-                                            <img src={e.img} className='w-12' />
-                                            <p className="mt-3 text-center">{e.name}</p>
-                                        </div>
-                                    ))
-                                }
-                            </div>
-                    }
-
-                    {
-                        postType != 1 && (
-                            <div className="fixed bottom-20 md:bottom-0 md:mt-6 md:relative w-full px-3 md:px-32">
-                                <button onClick={() => onCheckHandle()} className=" bg-primary w-full text-center hover:bg-green-500 text-white rounded-full py-3 px-4">
-                                    Check My Tools
-                                </button>
-                            </div>)
-                    }
-                    {/* notice */}
-                    <div className="px-3 md:px-32">
-                        <div className="mt-8 relative w-full border border-dashed border-primary bg-primary/10">
-                            <span className="-top-3 absolute bg-secondry text-white ml-2 px-2 rounded">
-                                {postType == 1 ? 'Information' : 'Notice'}
-                            </span>
-                            {postType == 1
-                                ?
-                                <p className="text-center p-6 text-secondry text-sm">All Images From Internet, Use Here Only For Education & Training Purpose <Link href='/' className="text-primary">Know more</Link></p>
+                            )
+                        }
+                        {
+                            postType == 1 ?
+                                <ul className="grid grid-cols-2 px-3 gap-3">
+                                    {tools.map((e, index) => (
+                                        <li className="relative" onClick={() => setPopup2(`${e.img}`)}>
+                                            <img src={e.img} className='rounded-lg h-32 w-full object-cover filter-none contrast-900' />
+                                            <h2 className="absolute bottom-2 left-2 text-sm">{e.name}</h2>
+                                        </li>
+                                    ))}
+                                </ul>
                                 :
-                                <p className="text-center p-6 text-secondry text-sm">This Tools Help For Check Your All Equipment.<br /><b>Before Going To Work.</b> </p>
-                            }
+                                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-3 md:px-32">
+                                    {
+                                        tools.map((e, index) => (
+                                            <div
+                                                onClick={() => {
+                                                    handlechange(index);
+                                                }}
+                                                key={index}
+                                                className={`${e.pos == 'act' && `border-primary border-2`} grid items-center justify-items-center border-2 p-4 md:p-6 rounded-lg`}>
+                                                <img src={e.img} className='w-12' />
+                                                <p className="mt-3 text-center">{e.name}</p>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                        }
 
+                        {
+                            postType != 1 && (
+                                <div className="fixed bottom-20 md:bottom-0 md:mt-6 md:relative w-full px-3 md:px-32">
+                                    <button onClick={() => onCheckHandle()} className=" bg-primary w-full text-center hover:bg-green-500 text-white rounded-full py-3 px-4">
+                                        Check My Tools
+                                    </button>
+                                </div>)
+                        }
+                        {/* notice */}
+                        <div className="px-3 md:px-32">
+                            <div className="mt-8 relative w-full border border-dashed border-primary bg-primary/10">
+                                <span className="-top-3 absolute bg-secondry text-white ml-2 px-2 rounded">
+                                    {postType == 1 ? 'Information' : 'Notice'}
+                                </span>
+                                {postType == 1
+                                    ?
+                                    <p className="text-center p-6 text-secondry text-sm">All Images From Internet, Use Here Only For Education & Training Purpose <Link href='/' className="text-primary">Know more</Link></p>
+                                    :
+                                    <p className="text-center p-6 text-secondry text-sm">This Tools Help For Check Your All Equipment.<br /><b>Before Going To Work.</b> </p>
+                                }
+
+                            </div>
                         </div>
-                    </div>
 
-                    {/* toast & modal box */}
-                    <Toaster
-                        position="bottom-center"
-                        reverseOrder={false}
-                    />
-                    <ToolsModalBox showModel={popup} closeModel={setPopup} avail={tools.filter(i => i.pos == 'act').map((e) => (e.name))} Notavail={tools.filter(i => i.pos != 'act').map((e) => (e.name))} />
+                        {/* toast & modal box */}
+                        <Toaster
+                            position="bottom-center"
+                            reverseOrder={false}
+                        />
+                        <ToolsModalBox showModel={popup} closeModel={setPopup} avail={tools.filter(i => i.pos == 'act').map((e) => (e.name))} Notavail={tools.filter(i => i.pos != 'act').map((e) => (e.name))} />
 
-                    <ImageView showModel={popup1} closeModel={setPopup2} />
-                </div>}
+                        <ImageView showModel={popup1} closeModel={setPopup2} />
+                    </div> :
+                    <p className="mt-24 text-center">not found, maybe wrong URL</p>
+            }
             <div className="w-1/4">
 
             </div>
--- pages/tools/home.js
+++ pages/tools/home.js
@@ -4,11 +4,19 @@ export default function ToolHome() {
 
     const tool = [
         {
-            name: 'Split AC Install',
-            urlKey: 'acInstall'
+            name: 'AC Installation',
+            urlKey: 'ACInstallationTools'
         },
         {
             name: 'Gas Refilling',
+            urlKey: 'acGasRefilling'
+        },
+        {
+            name: 'AC Dismantle',
+            urlKey: 'acGasRefilling'
+        },
+        {
+            name: 'AC Servicing',
             urlKey: 'acGasRefilling'
         },
 
@@ -17,56 +25,100 @@ export default function ToolHome() {
     const InstallIdea = [
         {
             name: 'Split AC (IDU)',
-            urlKey: 'ideaIDU'
+            urlKey: 'iduInstallIdea'
         },
         {
             name: 'Split AC (ODU)',
-            urlKey: 'acGasRefilling'
+            urlKey: 'oduInstallIdea'
+        },
+        {
+            name: 'Split AC (IDU)',
+            urlKey: 'iduInstallIdea'
+        },
+        {
+            name: 'Split AC (ODU)',
+            urlKey: 'oduInstallIdea'
         },
 
     ]
     return (
         <>
 
-            <div className="pt-6 w-full font-[Urbanist] px-3 md:px-32">
-                <h1 className="font-extrabold">Select Your Daily Use Tools</h1>
-                <div className="mt-6 grid grid-cols-2 md:grid-cols-3 gap-3">
-                    {
-                        tool.map((e, index) => (
-                            <Link
-                                key={index}
-                                href={{
-                                    pathname: '/tools/get/tools-list',
-                                    query: { key: `${e.urlKey}` },
-                                }}
-                            >
-                                <div className="bg-primary/30 rounded border-2 border-primary h-32 w-full grid items-center text-center">
-                                    <h2>{e.name}</h2>
-                                </div>
-                            </Link>
-                        ))
-                    }
-
+            <div className="pt-6 pb-24 w-full font-[Urbanist] px-3 md:px-32">
+                <div className="bg-black text-white py-4 rounded-md">
+                    <h1 className="text-center">Technician Assistant</h1>
                 </div>
 
-                <h1 className="mt-6 font-extrabold">AC Installation Ideas</h1>
-                <div className="mt-6 grid grid-cols-2 md:grid-cols-3 gap-3">
-                    {
-                        InstallIdea.map((e, index) => (
-                            <Link
-                                key={index}
-                                href={{
-                                    pathname: '/tools/get/tools-list',
-                                    query: { key: `${e.urlKey}` },
-                                }}
-                            >
-                                <div className="bg-primary/30 rounded border-2 border-primary h-32 w-full grid items-center text-center">
-                                    <h2>{e.name}</h2>
-                                </div>
-                            </Link>
-                        ))
-                    }
+                <div>
+                    <h2 className="mt-6 font-extrabold">Daily Use Tools</h2>
+                    <div className="border-t mt-8 mb-8 relative">
+                        <span className="-top-4 left-2 text-sm absolute bg-white px-4 py-1 rounded-full border">Split</span>
+                    </div>
+                    <div className="mt-4 grid grid-cols-2 md:grid-cols-3 gap-3">
+                        {
+                            tool.map((e, index) => (
+                                <Link
+                                    key={index}
+                                    href={{
+                                        pathname: '/tools/get/tools-list',
+                                        query: { key: `${e.urlKey}` },
+                                    }}
+                                >
+                                    <div className="bg-primary/30 rounded border-2 border-primary h-32 w-full grid items-center text-center">
+                                        <h2>{e.name}</h2>
+                                    </div>
+                                </Link>
+                            ))
+                        }
+                    </div>
+                </div>
 
+
+                <div className="bg-primary shadow-xl py-6 -mx-3 px-4 mt-6">
+                    <h2 className="font-extrabold text-white text-center">AC Installation Ideas</h2>
+                    <div className="mt-4 grid grid-cols-2 md:grid-cols-3 gap-3 overflow-y-scroll h-16">
+                        {
+                            InstallIdea.map((e, index) => (
+                                <Link
+                                    key={index}
+                                    href={{
+                                        pathname: '/tools/get/tools-list',
+                                        query: { key: `${e.urlKey}` },
+                                    }}
+                                >
+                                    <div className="bg-white rounded-lg border-2 border-primary h-16 w-full grid items-center text-center">
+                                        <h2>{e.name}</h2>
+                                    </div>
+                                </Link>
+                            ))
+                        }
+                    </div>
+                    <div className="border-t mt-8 mb-2 relative">
+                        <span className="-top-4 left-1/3 text-sm ml-2 absolute bg-white px-4 py-1 rounded-full border">Show More</span>
+                    </div>
+                </div>
+
+                <div>
+                    <div className="border-t mt-8 mb-8 relative">
+                        <span className="-top-4 left-2 text-sm absolute bg-white px-4 py-1 rounded-full border">Window</span>
+                    </div>
+                    <div className="mt-4 grid grid-cols-2 md:grid-cols-3 gap-3">
+                        {
+                            tool.map((e, index) => (
+                                <Link
+                                    key={index}
+                                    href={{
+                                        pathname: '/tools/get/tools-list',
+                                        query: { key: `${e.urlKey}` },
+                                    }}
+                                >
+                                    <div className="bg-primary/30 rounded border-2 border-primary h-32 w-full grid items-center text-center">
+                                        <h2>{e.name}</h2>
+                                    </div>
+                                </Link>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
 
